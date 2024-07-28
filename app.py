e08e1aefe9498f7537ba3beedf451d9bbefc903c
OlavoFerrazNeto <olavoferrazn@gmail.com>
2024-07-28 15:10:17 -0300

    Testrr

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-print("TESTE")
+import streamlit as st
+import pandas as pd
+from bcb import currency
+
+st.title("Testee")
+
+st.markdown("Objetivo do Dash")
+
+sel_ano = st.radio(
+    label = "Selecione o ano:",
+    options=["2022","2023","2024"],
+    index=None,
+)
+
+st.write("Você selecionou:",sel_ano)
